2e5be72f95ce5517e00ffdc99a563e5a8d8e50ae
Timothy Baldridge <tbaldridge@gmail.com>
2020-05-11 21:35:14 -0600

    Rework how we handle file magic, I'd like to apply this to other file detection routines we have

--- Compression.BSA/BSADispatch.cs
+++ Compression.BSA/BSADispatch.cs
@@ -2,34 +2,30 @@
 using System.IO;
 using System.Text;
 using Wabbajack.Common;
+using Wabbajack.Common.FileSignatures;
 
 namespace Compression.BSA
 {
     public static class BSADispatch
     {
+        private static readonly SignatureChecker Signature = new SignatureChecker(Definitions.FileType.BSA, Definitions.FileType.BA2, Definitions.FileType.TES3);
         public static IBSAReader OpenRead(AbsolutePath filename)
         {
-            var fourcc = "";
-            using (var file = filename.OpenRead())
+            switch (Signature.Matches(filename))
             {
-                fourcc = Encoding.ASCII.GetString(new BinaryReader(file).ReadBytes(4));
+                case Definitions.FileType.TES3:
+                    return new TES3Reader(filename);
+                case Definitions.FileType.BSA:
+                    return new BSAReader(filename);
+                case Definitions.FileType.BA2:
+                    return new BA2Reader(filename);
+                default:
+                    throw new InvalidDataException("Filename is not a .bsa or .ba2, magic " + filename);
             }
-
-            if (fourcc == TES3Reader.TES3_MAGIC)
-                return new TES3Reader(filename);
-            if (fourcc == "BSA\0")
-                return new BSAReader(filename);
-            if (fourcc == "BTDX")
-                return new BA2Reader(filename);
-            throw new InvalidDataException("Filename is not a .bsa or .ba2, magic " + fourcc);
         }
-
-        private static HashSet<string> MagicStrings = new HashSet<string> {TES3Reader.TES3_MAGIC, "BSA\0", "BTDX"};
         public static bool MightBeBSA(AbsolutePath filename)
         {
-            using var file = filename.OpenRead();
-            var fourcc = Encoding.ASCII.GetString(new BinaryReader(file).ReadBytes(4));
-            return MagicStrings.Contains(fourcc);
+            return Signature.Matches(filename).HasValue;
         }
     }
 }
