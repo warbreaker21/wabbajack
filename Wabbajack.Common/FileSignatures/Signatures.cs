4fd88e8902e698a8efc761423a5c60222be1e65b
Timothy Baldridge <tbaldridge@gmail.com>
2020-05-27 14:32:13 -0600

    Add DDS file signature

--- Wabbajack.Common/FileSignatures/Signatures.cs
+++ Wabbajack.Common/FileSignatures/Signatures.cs
@@ -1,7 +1,8 @@
 
 namespace Wabbajack.Common.FileSignatures {
     public static class Definitions {
-    public enum FileType {       _123,
+    public enum FileType {        DDS,
+          _123,
           _386,
           _3G2,
           _3GG,
@@ -439,6 +440,9 @@ namespace Wabbajack.Common.FileSignatures {
 
                 // FO4 BSA
         (FileType.BA2, new byte[] {0x42, 0x54, 0x44, 0x58}),
+
+                // Direct Draw Surface
+        (FileType. DDS, new byte[] {0x44, 0x44, 0x53, 0x20}),
 
                 // JPEG2000 image files
         (FileType.JP2, new byte[] {0x00, 0x00, 0x00, 0x0C, 0x6A, 0x50, 0x20, 0x20}),
